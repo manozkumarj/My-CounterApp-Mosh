03546ff3af4730456c2496f7df30e81c529fd792
Manoj Kumar <manojmani517@gmail.com>
2019-11-16 22:04:12 +0530

    Added handleIncrement event and function

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,17 +2,47 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3", "tag4", "tag5"]
+  };
+
+  rederTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags available</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}> {tag} </li>
+        ))}
+      </ul>
+    );
+  }
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <div>
         <span className={this.getClass()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary m-2">Increment</button>
+        <button
+          className="btn btn-secondary m-2"
+          onClick={this.handleIncrement}
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && <h5>Please create new tags</h5>}
+        {this.rederTags()}
       </div>
     );
   }
+
   getClass() {
     let { count } = this.state;
     let classes = "btn m-2 btn-";
